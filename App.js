2e6d9fcf66c1e58866370401be0f2827f7adcca3
Stuart Casarotto <stuart.casarotto@38thstreetstudios.com>
2023-01-08 15:12:59 -0600

    updated app to send to phone and made to do list

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react'
-import { StatusBar } from 'expo-status-bar'
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
 import {
-	StyleSheet,
-	Text,
-	View,
-	ScrollView,
-	Image,
-	SafeAreaView,
-	Pressable,
-	FlatList,
-} from 'react-native'
-import { episodes, episodesBySeason } from './episodes'
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  SafeAreaView,
+  Pressable,
+  FlatList,
+} from 'react-native';
+import { episodes, episodesBySeason } from './episodes';
 
 // import axios from 'axios'
 
@@ -46,92 +46,91 @@ import { episodes, episodesBySeason } from './episodes'
 // }, [])
 
 export default function App() {
-	const [pageMode, setPageMode] = useState('randomizer')
-	const [selectedEpisode, setSelectedEpisode] = useState()
-	const [selectedSeasonNumber, setSelectedSeasonNumber] = useState()
+  const [pageMode, setPageMode] = useState('randomizer');
+  const [selectedEpisode, setSelectedEpisode] = useState();
+  const [selectedSeasonNumber, setSelectedSeasonNumber] = useState();
 
-	if (pageMode === 'randomizer') {
-		return (
-			<SafeAreaView style={styles.background}>
-				<StatusBar style='light' />
-				<Text style={styles.randomizeTitle}>Get a random episode!</Text>
-				{selectedEpisode && (
-					<>
-						<Image
-							source={{ uri: selectedEpisode.thumbnail_url }}
-							style={styles.selectedEpisodeImage}
-						/>
-						<Text style={styles.selectedEpisodeTitle}>
-							S{selectedEpisode.season}E{selectedEpisode.episode}:{' '}
-							{selectedEpisode.name}
-						</Text>
-					</>
-				)}
-				<Pressable
-					style={styles.randomizeButton}
-					onPress={() => {
-						setSelectedEpisode(episodes[Math.floor(Math.random() * episodes.length)])
-					}}
-				>
-					<Text style={styles.randomizeButtonText}>
-						{!!selectedEpisode ? 'Give Me Another One Guy!' : 'Random Episode'}
-					</Text>
-				</Pressable>
-				<Pressable
-					style={styles.navigateButton}
-					onPress={() => {
-						setPageMode('seasons')
-						setSelectedEpisode(undefined)
-					}}
-				>
-					<Text style={styles.navigateButtonText}>View Seasons</Text>
-				</Pressable>
-			</SafeAreaView>
-		)
-	}
+  if (pageMode === 'randomizer') {
+    return (
+      <SafeAreaView style={styles.background}>
+        <StatusBar style='light' />
+        <Text style={styles.randomizeTitle}>Get a random episode!</Text>
+        {selectedEpisode && (
+          <>
+            <Image
+              source={{ uri: selectedEpisode.thumbnail_url }}
+              style={styles.selectedEpisodeImage}
+            />
+            <Text style={styles.selectedEpisodeTitle}>
+              S{selectedEpisode.season}E{selectedEpisode.episode}: {selectedEpisode.name}
+            </Text>
+          </>
+        )}
+        <Pressable
+          style={styles.randomizeButton}
+          onPress={() => {
+            setSelectedEpisode(episodes[Math.floor(Math.random() * episodes.length)]);
+          }}
+        >
+          <Text style={styles.randomizeButtonText}>
+            {!!selectedEpisode ? 'Give Me Another One Guy!' : 'Random Episode'}
+          </Text>
+        </Pressable>
+        <Pressable
+          style={styles.navigateButton}
+          onPress={() => {
+            setPageMode('seasons');
+            setSelectedEpisode(undefined);
+          }}
+        >
+          <Text style={styles.navigateButtonText}>View Seasons</Text>
+        </Pressable>
+      </SafeAreaView>
+    );
+  }
 
-	return (
-		<SafeAreaView style={styles.background}>
-			<StatusBar style='light' />
-			<View style={styles.seasonHeader}>
-				<Text style={styles.seasonHeaderTitle}>Select a Season</Text>
-				<View style={styles.seasonBlockWrapper}>
-					{episodesBySeason.map((_, index) => (
-						<Pressable
-							style={[
-								styles.seasonNumberButton,
-								selectedSeasonNumber === index && styles.selectedSeasonNumberButton,
-							]}
-							onPress={() => setSelectedSeasonNumber(index)}
-						>
-							<Text style={styles.seasonNumberButtonText}>{index + 1}</Text>
-						</Pressable>
-					))}
-				</View>
-				<Pressable
-					style={styles.seasonsBackButton}
-					onPress={() => {
-						setPageMode('randomizer')
-						setSelectedSeasonNumber(0)
-					}}
-				>
-					<Text style={styles.seasonsBackButtonText}>Back</Text>
-				</Pressable>
-			</View>
-			<FlatList
-				style={styles.episodeList}
-				data={episodesBySeason[selectedSeasonNumber]}
-				renderItem={({ item }) => (
-					<View style={styles.episodeWrapper}>
-						<Image source={{ uri: item.thumbnail_url }} style={styles.episodeImage} />
-						<Text style={styles.episodeName}>
-							E{item.episode}: {item.name}
-						</Text>
-					</View>
-				)}
-				keyExtractor={(item) => item.id}
-			/>
-			{/* <ScrollView>
+  return (
+    <SafeAreaView style={styles.background}>
+      <StatusBar style='light' />
+      <View style={styles.seasonHeader}>
+        <Text style={styles.seasonHeaderTitle}>Select a Season</Text>
+        <View style={styles.seasonBlockWrapper}>
+          {episodesBySeason.map((_, index) => (
+            <Pressable
+              style={[
+                styles.seasonNumberButton,
+                selectedSeasonNumber === index && styles.selectedSeasonNumberButton,
+              ]}
+              onPress={() => setSelectedSeasonNumber(index)}
+            >
+              <Text style={styles.seasonNumberButtonText}>{index + 1}</Text>
+            </Pressable>
+          ))}
+        </View>
+        <Pressable
+          style={styles.seasonsBackButton}
+          onPress={() => {
+            setPageMode('randomizer');
+            setSelectedSeasonNumber(0);
+          }}
+        >
+          <Text style={styles.seasonsBackButtonText}>Back</Text>
+        </Pressable>
+      </View>
+      <FlatList
+        style={styles.episodeList}
+        data={episodesBySeason[selectedSeasonNumber]}
+        renderItem={({ item }) => (
+          <View style={styles.episodeWrapper}>
+            <Image source={{ uri: item.thumbnail_url }} style={styles.episodeImage} />
+            <Text style={styles.episodeName}>
+              E{item.episode}: {item.name}
+            </Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+      />
+      {/* <ScrollView>
 				<View style={styles.container}>
 					<StatusBar style='light' />
 					<Text style={styles.title}>Welcome to my very cool south park app!</Text>
@@ -156,122 +155,122 @@ export default function App() {
 					})}
 				</View>
 			</ScrollView> */}
-		</SafeAreaView>
-	)
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-	background: {
-		backgroundColor: '#111111',
-		flex: 1,
-		alignItems: 'center',
-		flexDirection: 'column',
-		justifyContent: 'center',
-	},
-	randomizeTitle: {
-		color: '#ffffff',
-		fontSize: 20,
-		marginBottom: 20,
-	},
-	selectedEpisodeImage: {
-		width: 200,
-		height: 200,
-		marginBottom: 20,
-		borderRadius: 10,
-	},
-	selectedEpisodeTitle: {
-		color: '#ffffff',
-		fontSize: 24,
-		marginBottom: 20,
-		textAlign: 'center',
-	},
-	randomizeButton: {
-		backgroundColor: 'rgba(255,255,255,0.2)',
-		padding: 14,
-		borderRadius: 20,
-		marginBottom: 10,
-	},
-	randomizeButtonText: {
-		color: '#ffffff',
-		fontSize: 18,
-	},
-	navigateButton: {
-		padding: 14,
-		borderRadius: 20,
-	},
-	navigateButtonText: {
-		color: '#ffffff',
-		fontSize: 18,
-	},
+  background: {
+    backgroundColor: '#111111',
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  randomizeTitle: {
+    color: '#ffffff',
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  selectedEpisodeImage: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+    borderRadius: 10,
+  },
+  selectedEpisodeTitle: {
+    color: '#ffffff',
+    fontSize: 24,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  randomizeButton: {
+    backgroundColor: 'rgba(255,255,255,0.2)',
+    padding: 14,
+    borderRadius: 20,
+    marginBottom: 10,
+  },
+  randomizeButtonText: {
+    color: '#ffffff',
+    fontSize: 18,
+  },
+  navigateButton: {
+    padding: 14,
+    borderRadius: 20,
+  },
+  navigateButtonText: {
+    color: '#ffffff',
+    fontSize: 18,
+  },
 
-	seasonHeader: {
-		marginBottom: 20,
-		paddingHorizontal: 10,
-	},
-	seasonHeaderTitle: {
-		color: '#ffffff',
-		fontSize: 20,
-		marginBottom: 20,
-		textAlign: 'center',
-	},
-	seasonBlockWrapper: {
-		flexDirection: 'row',
-		flexWrap: 'wrap',
-		justifyContent: 'center',
-	},
-	seasonNumberButton: {
-		backgroundColor: 'rgba(255,255,255,0.2)',
-		minWidth: 35,
-		minHeight: 35,
-		justifyContent: 'center',
-		alignItems: 'center',
-		margin: 3,
-		borderRadius: 3,
-	},
-	selectedSeasonNumberButton: {
-		backgroundColor: 'rgba(255,255,255,0.5)',
-	},
-	seasonNumberButtonText: {
-		color: '#ffffff',
-		textAlign: 'center',
-		fontSize: 16,
-		fontWeight: '600',
-	},
-	seasonsBackButton: {
-		position: 'absolute',
-		top: -15,
-		left: 0,
-		paddingHorizontal: 15,
-		paddingVertical: 20,
-	},
-	seasonsBackButtonText: { color: '#ffffff' },
-	episodeList: { width: '100%' },
-	container: {
-		flex: 1,
-	},
-	title: {
-		fontSize: 20,
-		textAlign: 'center',
-		margin: 10,
-		color: '#ffffff',
-	},
-	episodeWrapper: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'flex-start',
-		paddingLeft: 10,
-		paddingRight: 10,
-		marginBottom: 10,
-	},
-	episodeImage: {
-		width: 75,
-		height: 75,
-		borderRadius: 5,
-	},
-	episodeName: {
-		marginLeft: 10,
-		fontSize: 18,
-		color: '#ffffff',
-		flexShrink: 1,
-	},
-})
+  seasonHeader: {
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+  seasonHeaderTitle: {
+    color: '#ffffff',
+    fontSize: 20,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  seasonBlockWrapper: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  seasonNumberButton: {
+    backgroundColor: 'rgba(255,255,255,0.2)',
+    minWidth: 35,
+    minHeight: 35,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 3,
+    borderRadius: 3,
+  },
+  selectedSeasonNumberButton: {
+    backgroundColor: 'rgba(255,255,255,0.5)',
+  },
+  seasonNumberButtonText: {
+    color: '#ffffff',
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  seasonsBackButton: {
+    position: 'absolute',
+    top: -15,
+    left: 0,
+    paddingHorizontal: 15,
+    paddingVertical: 20,
+  },
+  seasonsBackButtonText: { color: '#ffffff' },
+  episodeList: { width: '100%' },
+  container: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+  },
+  episodeWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginBottom: 10,
+  },
+  episodeImage: {
+    width: 75,
+    height: 75,
+    borderRadius: 5,
+  },
+  episodeName: {
+    marginLeft: 10,
+    fontSize: 18,
+    color: '#ffffff',
+    flexShrink: 1,
+  },
+});
---
+++ TODO.md
@@ -0,0 +1,12 @@
+# TODOs
+
+- [ ] Update our data
+  - [ ] Check if dude took down his API or something
+  - [ ] Download the new data
+- [ ] Write a script to download images
+- [ ] App Icon
+- [ ] Loading Screen
+- [ ] Home Background
+- [ ] Advanced Search
+  - [ ] Christmas Episodes
+  - [ ] ... etc
